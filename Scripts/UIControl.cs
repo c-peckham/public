39408358942fbca9d7376e8dc46726281b2ad67a
c-peckham <58100839+c-peckham@users.noreply.github.com>
2020-10-01 02:02:00 -0400

    Add files via upload

---
+++ Scripts/UIControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+	public bool m_UseRelativeRotation = true;
+
+	private Quaternion m_RelativeRotation;
+
+	private void Start()
+	{
+		m_RelativeRotation = transform.parent.localRotation;
+	}
+
+	private void Upate ()
+	{
+		if (m_UseRelativeRotation)
+			transform.rotation = m_RelativeRotation;
+	}
+			
+}
